f8acd4caa3b1a300a9c3a7d72fe182def50a9537
elijahhawkins-the-maker <elijah.hawkins@ucas-edu.net>
2026-04-13 22:21:01 -0600

    BRUH174

--- projects/portfolio/graphics.py
+++ projects/portfolio/graphics.py
@@ -1,13 +1,13 @@
 #EHCP2 where actual portfolio is made
+import sys
+
+sys.path.insert(0, "/home/elijahhawkins/EH_CP1-1/projects/fractal_gen")
+
 import tkinter as tk
 
-from fractal_gen.fg_main import main
+
 
 import DND
-
-from geo_calc.main import main as geo_calc_main
-
-from word_counter.main_wc import main_menu as word_counter_main
 
 class GUI():
     def __init__(self):
@@ -18,7 +18,7 @@ class GUI():
         self.label = tk.Label(self.root, text="Welcome to EHCP2, by Elijah Hawkins")
         self.label.pack()
 
-        self.button = tk.Button(self.root, text="Show Fractal Generator", command=main)
+        self.button = tk.Button(self.root, text="Show Fractal Generator", command=fractal_main)
         self.button2 = tk.Button(self.root, text="Show DND", command=DND.main)
         self.button3 = tk.Button(self.root, text="Show Geo Calc", command=geo_calc_main)
         self.button4 = tk.Button(self.root, text="Show Word Counter", command=word_counter_main)
@@ -39,4 +39,4 @@ class PortfolioGUI(GUI):
         self.label.pack()
         self.root.mainloop()
 
-PortfolioGUI()
+GUI()
